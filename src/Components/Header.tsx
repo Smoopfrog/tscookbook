a91a6621a02a5cd472b3e1567a225947271d9326
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-06-15 15:15:48 -0700

    create mobile menu aside

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "../Styles/Header.css";
 
 const Header = () => {
+  const [showMenuAside, setShowMenuAside] = useState<Boolean>(false);
+
   return (
     <header className="header">
-      <button>Search</button>
-      <h1>TS Cookbook</h1>
-      <button>Menu</button>
+      <div className="header-nav">
+        <button>Search</button>
+        <h1>TS Cookbook</h1>
+        <button onClick={() => setShowMenuAside((prev) => !prev)}>Menu</button>
+      </div>
+      <aside
+        className={`header-links ${showMenuAside ? "" : "hide-menu"}`}
+      >
+        <ul className="nav-list">
+          <li>Home</li>
+          <li>Recipes</li>
+          <li>Saved</li>
+          <li>Settings</li>
+        </ul>
+      </aside>
     </header>
   );
 };

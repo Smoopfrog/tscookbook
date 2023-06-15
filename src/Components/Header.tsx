5ec2185234734eca177071013d787c31c73fb741
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-06-15 15:27:26 -0700

    Create mobile search aside

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,23 +3,28 @@ import "../Styles/Header.css";
 
 const Header = () => {
   const [showMenuAside, setShowMenuAside] = useState<Boolean>(false);
+  const [showSearchAside, setShowSearchAside] = useState<Boolean>(false);
+
 
   return (
     <header className="header">
       <div className="header-nav">
-        <button>Search</button>
+        <button onClick={() => setShowSearchAside((prev) => !prev)}>Search</button>
         <h1>TS Cookbook</h1>
         <button onClick={() => setShowMenuAside((prev) => !prev)}>Menu</button>
       </div>
-      <aside
-        className={`header-links ${showMenuAside ? "" : "hide-menu"}`}
-      >
+      <aside className={`menu-aside ${showMenuAside ? "" : "hide-menu"}`}>
         <ul className="nav-list">
           <li>Home</li>
           <li>Recipes</li>
           <li>Saved</li>
           <li>Settings</li>
         </ul>
+      </aside>
+      <aside className={`search-aside ${showSearchAside ? "" : "hide-search"}`}>
+        <h1>Search for a recipe</h1>
+        <input placeholder="Cookies..." />
+        <button>Search</button>
       </aside>
     </header>
   );

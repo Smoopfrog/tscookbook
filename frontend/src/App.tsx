cabe8621c1b0aa37d9fb00870bf3c6c00b7a5749
Smoopfrog <jeffstinson@hotmail.com>
2023-06-21 23:19:25 -0700

    fetch data indata

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,54 @@
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import HomePage from "./Components/HomePage/HomePage";
 import NewRecipeForm from "./Components/NewRecipeForm";
-import Recipe from "./Components/Recipe";
-
-export interface RecipeInfo {
-  title: string;
-  category: string;
-  description: string;
-  ingredients: { amount: number; measurement?: string; name: string }[];
-  directions: string[];
-}
+// import RecipeComponent from "./Components/RecipeComponent";
+import { Recipe } from "./models/recipe";
+import RecipeComponent from "./Components/RecipeComponent";
 
 function App() {
   const [showRecipeForm, setShowRecipeForm] = useState<boolean>(false);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const loadRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes", { method: "GET" });
+        const recipes = await response.json();
+        setRecipes(recipes);
+      } catch (error) {
+        console.log(error);
+        alert(error);
+      }
+    };
+    loadRecipes();
+  }, []);
 
   const handleShowRecipeForm = (e: MouseEvent<HTMLButtonElement>) => {
     console.log("show form");
     setShowRecipeForm(!showRecipeForm);
   };
 
-  const recipe: RecipeInfo = {
-    title: "chocolate chip cookie",
-    category: "Cookie",
-    description: "The best cookie in the world",
-    ingredients: [
-      { amount: 2, measurement: undefined, name: "eggs" },
-      { amount: 2, measurement: "cups", name: "flour" },
-      { amount: 1, measurement: "cup", name: "butter" },
-    ],
-    directions: ["Put ingredient in bowl", "Mix Bowl", "Cook bowl", "Eat"],
-  };
+  // const recipe: RecipeInfo = {
+  //   title: "chocolate chip cookie",
+  //   category: "Cookie",
+  //   description: "The best cookie in the world",
+  //   ingredients: [
+  //     { amount: 2, measurement: undefined, name: "eggs" },
+  //     { amount: 2, measurement: "cups", name: "flour" },
+  //     { amount: 1, measurement: "cup", name: "butter" },
+  //   ],
+  //   directions: ["Put ingredient in bowl", "Mix Bowl", "Cook bowl", "Eat"],
+  // };
 
   return (
     <div className="App">
       <Header />
       <HomePage openForm={handleShowRecipeForm} />
+      {recipes.map((recipe) => {
+        return <RecipeComponent recipe={recipe} key={recipe._id}/>;
+      })}
       <dialog open={showRecipeForm}>
         <NewRecipeForm />
       </dialog>
---
+++ frontend/src/models/recipe.ts
@@ -0,0 +1,10 @@
+export interface Recipe {
+  _id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  // category?: string;
+  // ingredients?: { amount: number; measurement?: string; name: string }[];
+  // directions?: string[];
+}

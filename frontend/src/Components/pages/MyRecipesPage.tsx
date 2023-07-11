3fafb92f559abc92e14a496b7d04824d8ccdbe36
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-07-11 13:34:35 -0700

    add basic styles to myrecipes page

--- frontend/src/Components/pages/MyRecipesPage.tsx
+++ frontend/src/Components/pages/MyRecipesPage.tsx
@@ -1,14 +1,17 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import RecipeThumbnail from "../RecipeThumbnail";
 import { Recipe } from "../../models/recipe";
-import "../../Styles/MyRecipesPage.css"
+import "../../Styles/MyRecipesPage.css";
 
 const MyRecipesPage = () => {
   const recipes = useLoaderData() as Recipe[];
 
   return (
     <section className="my-recipes-page">
-      <h1>My Recipes</h1>
+      <header className="my-recipes-page-header">
+        <h1 className="my-recipes-page-title">My Recipes</h1>
+        <Link to="/newrecipe">Add Recipe</Link>
+      </header>
       <ul>
         {recipes.map((recipe) => {
           return (

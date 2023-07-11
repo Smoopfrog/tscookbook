02a9759aba228f54a91aceab14eec0b2219a801f
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-07-11 16:07:52 -0700

    changer header nav icon and fix layout

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "../Styles/Header.css";
 import { Link } from "react-router-dom";
-
+import { FiMenu } from "react-icons/fi";
 const Header = () => {
   const [showMenuAside, setShowMenuAside] = useState<Boolean>(false);
 
@@ -13,19 +13,33 @@ const Header = () => {
     <header className="header">
       <div className="header-nav">
         <h1>TS Cookbook</h1>
-        <button onClick={handleMenuAside}>Menu</button>
+        <button className="header-menu-btn" onClick={handleMenuAside}>
+          <FiMenu />
+        </button>
       </div>
       <aside className={`menu-aside ${showMenuAside ? "" : "hide-menu"}`}>
         <h2>My Cookbook</h2>
         <ul className="nav-list">
-          <Link className="header-nav-item" to="/home" onClick={handleMenuAside}>
+          <Link
+            className="header-nav-item"
+            to="/home"
+            onClick={handleMenuAside}
+          >
             Home
           </Link>
-          <Link className="header-nav-item" to="/myrecipes" onClick={handleMenuAside}>
-            My Recipes
-          </Link>
-          <Link className="header-nav-item" to="/newrecipe" onClick={handleMenuAside}>
+          <Link
+            className="header-nav-item"
+            to="/newrecipe"
+            onClick={handleMenuAside}
+          >
             New Recipe
+          </Link>
+          <Link
+            className="header-nav-item"
+            to="/myrecipes"
+            onClick={handleMenuAside}
+          >
+            My Recipes
           </Link>
           <Link className="header-nav-item" to="/">
             Search

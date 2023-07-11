98809171256869912d4e05a832dc21171ad74e6b
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-07-11 15:50:27 -0700

    style recipe page

--- frontend/src/Components/Recipe.tsx
+++ frontend/src/Components/Recipe.tsx
@@ -1,8 +1,11 @@
 import { ReactNode, MouseEvent } from "react";
 import { Recipe as RecipeModel } from "../models/recipe";
 import * as RecipesApi from "../network/recipes_api";
-import { useLoaderData, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useLoaderData, useNavigate, Link } from "react-router-dom";
+import { FaRegClock } from "react-icons/fa6";
+import { PiForkKnife } from "react-icons/pi";
+import { BiEdit } from "react-icons/bi";
+import "../Styles/Recipe.css";
 
 const Recipe = () => {
   const recipe = useLoaderData() as RecipeModel;
@@ -21,8 +24,9 @@ const Recipe = () => {
   const ingredientComponents: ReactNode = recipe.ingredients?.map(
     (ingredient, index) => {
       return (
-        <li key={index}>
-          {ingredient.amount} {ingredient.measurement} {ingredient.name}
+        <li className="recipe-ingredient" key={index}>
+          <span>{ingredient.amount} </span>
+          {ingredient.measurement} {ingredient.name}
         </li>
       );
     }
@@ -30,21 +34,47 @@ const Recipe = () => {
 
   const directionComponents: ReactNode = recipe.directions?.map(
     (direction, index) => {
-      return <li key={index}>{direction.text}</li>;
+      return (
+        <li key={index}>
+          <span className="recipe-direction-index">{index + 1}.</span>
+          <span className="recipe-direction-text">{direction.text}</span>
+        </li>
+      );
     }
   );
-  
+
   return (
-    <article>
+    <article className="recipe">
       <img src={recipe.imgURL} alt="A meaning full alt tag" />
-      <h1>{recipe.title}</h1>
-      <h6>Yield: {recipe.portion}</h6>
-      <h6>Cooktime: {recipe.cooktime}</h6>
-      <p>{recipe.description}</p>
-      {ingredientComponents && <ul>{ingredientComponents}</ul>}
-      {directionComponents && <ol>{directionComponents}</ol>}
-      <button onClick={deleteRecipe}>Delete</button>
-      <Link to={`/myrecipes/${recipe._id}/edit`}>Edit</Link>
+      <div className="recipe-info">
+        <h1>{recipe.title}</h1>
+        <div className="recipe-info-subtitles">
+          <div className="recipe-info-spec">
+            <FaRegClock />
+            <span>Time: {recipe.cooktime}</span>
+          </div>
+          <div className="recipe-info-spec">
+            <PiForkKnife />
+            <span>Yield: {recipe.portion}</span>
+          </div>
+        </div>
+        <p className="recipe-description">{recipe.description}</p>
+        <Link className="recipe-edit-icon" to={`/myrecipes/${recipe._id}/edit`}>
+          <BiEdit />
+        </Link>
+      </div>
+      {ingredientComponents && (
+        <div className="recipe-ingredients">
+          <h2>Ingredients</h2>
+          <ul className="recipe-ingredients-list">{ingredientComponents}</ul>
+        </div>
+      )}
+      {directionComponents && (
+        <div className="recipe-directions">
+          <h2>Directions</h2>
+          <ol>{directionComponents}</ol>
+        </div>
+      )}
     </article>
   );
 };

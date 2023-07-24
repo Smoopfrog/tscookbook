064f33e85031f421a7201a000039ae580bb53fc8
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-07-23 23:59:54 -0700

    fetch logged in user upon open

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
+import * as UsersApi from "./network/users_api";
+import { useAppDispatch } from "./hooks";
+import { login } from "./slices/userSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const user = await UsersApi.getLoggedInUser();
+        dispatch(login(user));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLoggedInUser();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />

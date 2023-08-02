39c460e796f460e1ed5006e111e0aa4cd9ef77a0
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-08-02 11:43:10 -0700

    create homepage header

---
+++ frontend/src/Components/pages/HomepageHeader.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import "../../Styles/Header.css";
+import { Link } from "react-router-dom";
+
+const HomepageHeader = () => {
+  const [header, setHeader] = useState("");
+  const listenScrollEvent = () => {
+    if (window.scrollY > 0) {
+      return setHeader("");
+    } else if (window.scrollY < 1) {
+      return setHeader("header-top");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+
+    return () => window.removeEventListener("scroll", listenScrollEvent);
+  }, []);
+
+  return (
+    <header className={`header ${header}`}>
+      <div className="header-nav">
+        <Link to="/" className="header-title-link">
+          TS Cookbook
+        </Link>
+        <Link to="/" className="header-link">
+          Get Started
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default HomepageHeader;

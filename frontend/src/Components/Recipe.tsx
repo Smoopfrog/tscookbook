c2305adfb23541f5b0f25097179da8186dcf92b7
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-08-06 14:44:42 -0700

    change recipe style

--- frontend/src/Components/Recipe.tsx
+++ frontend/src/Components/Recipe.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Recipe as RecipeModel } from "../models/recipe";
 import { useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaRegClock } from "react-icons/fa6";
@@ -9,8 +9,22 @@ import { MouseEvent } from "react";
 import "../Styles/Recipe.css";
 
 const Recipe = () => {
+  const [carouselPage, setCarouselPage] = useState("About");
+  const handleClickScroll = (page: string) => {
+    const element = document.getElementById(page);
+
+    if (element) {
+      setCarouselPage(page);
+
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center",
+      });
+    }
+  };
   const recipe = useLoaderData() as RecipeModel;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const ingredientComponents: ReactNode = recipe.ingredients?.map(
     (ingredient, index) => {
@@ -46,39 +60,63 @@ const Recipe = () => {
 
   return (
     <article className="recipe">
-      <img src={recipe.imgURL} alt="A meaning full alt tag" />
-      <div className="recipe-info">
-        <h1>{recipe.title}</h1>
-        <div className="recipe-info-subtitles">
-          <div className="recipe-info-spec">
-            <FaRegClock />
-            <span>Time: {recipe.cooktime}</span>
+      <div className="recipe-carousel">
+        <section id="About">
+          <img src={recipe.imgURL} alt="A meaning full alt tag" />
+          <div className="recipe-info">
+            <h1>{recipe.title}</h1>
+            <div className="recipe-info-subtitles">
+              <div className="recipe-info-spec">
+                <FaRegClock />
+                <span>Time: {recipe.cooktime}</span>
+              </div>
+              <div className="recipe-info-spec">
+                <PiForkKnife />
+                <span>Yield: {recipe.portion}</span>
+              </div>
+            </div>
+            <p className="recipe-description">{recipe.description}</p>
+            <button className="recipe-delete-icon" onClick={deleteRecipe}>
+              <BiTrash />
+            </button>
+            <Link
+              className="recipe-edit-icon"
+              to={`/myrecipes/${recipe._id}/edit`}
+            >
+              <BiEdit />
+            </Link>
           </div>
-          <div className="recipe-info-spec">
-            <PiForkKnife />
-            <span>Yield: {recipe.portion}</span>
-          </div>
-        </div>
-        <p className="recipe-description">{recipe.description}</p>
-        <button className="recipe-delete-icon" onClick={deleteRecipe}>
-          <BiTrash />
-        </button>
-        <Link className="recipe-edit-icon" to={`/myrecipes/${recipe._id}/edit`}>
-          <BiEdit />
-        </Link>
+        </section>
+        <section id="Ingredients">
+          {ingredientComponents && (
+            <div className="recipe-ingredients">
+              <h2>Ingredients</h2>
+              <ul className="recipe-ingredients-list">
+                {ingredientComponents}
+              </ul>
+            </div>
+          )}
+        </section>
+        <section id="Directions">
+          {directionComponents && (
+            <div className="recipe-directions">
+              <h2>Directions</h2>
+              <ol>{directionComponents}</ol>
+            </div>
+          )}
+        </section>
       </div>
-      {ingredientComponents && (
-        <div className="recipe-ingredients">
-          <h2>Ingredients</h2>
-          <ul className="recipe-ingredients-list">{ingredientComponents}</ul>
-        </div>
-      )}
-      {directionComponents && (
-        <div className="recipe-directions">
-          <h2>Directions</h2>
-          <ol>{directionComponents}</ol>
-        </div>
-      )}
+      <footer className="recipe-form-footer">
+        <button onClick={() => handleClickScroll("About")} type="button">
+          About
+        </button>
+        <button onClick={() => handleClickScroll("Ingredients")} type="button">
+          Ingredients
+        </button>
+        <button onClick={() => handleClickScroll("Directions")} type="button">
+          Directions
+        </button>
+      </footer>
     </article>
   );
 };
--- frontend/src/Components/pages/RecipeForm.tsx
+++ frontend/src/Components/pages/RecipeForm.tsx
@@ -15,7 +15,7 @@ import { useState } from "react";
 
 const RecipeForm = () => {
   const [carouselPage, setCarouselPage] = useState("About");
-
+  
   const navigate = useNavigate();
   const recipe = useLoaderData() as Recipe;
 
@@ -96,170 +96,168 @@ const RecipeForm = () => {
           <BsCheckLg />
         </button>
       </header>
-        <div className="recipe-carousel">
-          <section
-            id="About"
-            className={`${carouselPage !== "About" && "hide-section"}`}
-          >
-            <div className="recipe-input-div">
-              <label>
-                Name
-                {errors.title && (
-                  <span className="recipe-form-err">
-                    This field is required
-                  </span>
-                )}
-              </label>
-              <input
-                placeholder="name"
-                {...register("title", { required: true })}
-              />
-            </div>
-            <div className="recipe-input-div">
-              <label>Description</label>
-              <input placeholder="description" {...register("description")} />
-            </div>
-            <div className="recipe-input-div">
-              <label>Yield</label>
-              <input placeholder="portion" {...register("portion")} />
-            </div>
-            <div className="recipe-input-div">
-              <label>Cooktime</label>
-              <input placeholder="cooktime" {...register("cooktime")} />
-            </div>
-            <div className="recipe-input-div">
-              <label>Image URL</label>
-              <input placeholder="Image URL" {...register("imgURL")} />
-            </div>
-          </section>
-          <section
-            id="Ingredients"
-            className={`recipe-form-ingredients ${
-              carouselPage !== "Ingredients" && "hide-section"
-            }`}
-          >
-            <h1>Ingredients</h1>
-            <ul>
-              {ingredientFields.map((ingredient, index) => {
-                return (
-                  <li className="recipe-form-ingredient" key={ingredient.id}>
-                    <div className="recipe-form-ingredient-inputs">
-                      <div className="recipe-form-ingredient-input">
-                        <label>Amount</label>
-                        <input
-                          placeholder="Amount"
-                          {...register(`ingredients.${index}.amount`, {
-                            required: true,
-                          })}
-                        />
-                      </div>
-                      <div className="recipe-form-ingredient-input">
-                        <label>Ingredient</label>
-                        <input
-                          placeholder="Ingredient"
-                          type="text"
-                          {...register(`ingredients.${index}.name`, {
-                            required: true,
-                          })}
-                        />
-                      </div>
-                      <button
-                        className="recipe-form-ingredient-delete"
-                        type="button"
-                        onClick={() => ingredientRemove(index)}
-                      >
-                        <BsTrash className="recipe-form-delete-icon" />
-                      </button>
+      <div className="recipe-carousel">
+        <section
+          id="About"
+          className={`${carouselPage !== "About" && "hide-section"}`}
+        >
+          <div className="recipe-input-div">
+            <label>
+              Name
+              {errors.title && (
+                <span className="recipe-form-err">This field is required</span>
+              )}
+            </label>
+            <input
+              placeholder="name"
+              {...register("title", { required: true })}
+            />
+          </div>
+          <div className="recipe-input-div">
+            <label>Description</label>
+            <input placeholder="description" {...register("description")} />
+          </div>
+          <div className="recipe-input-div">
+            <label>Yield</label>
+            <input placeholder="portion" {...register("portion")} />
+          </div>
+          <div className="recipe-input-div">
+            <label>Cooktime</label>
+            <input placeholder="cooktime" {...register("cooktime")} />
+          </div>
+          <div className="recipe-input-div">
+            <label>Image URL</label>
+            <input placeholder="Image URL" {...register("imgURL")} />
+          </div>
+        </section>
+        <section
+          id="Ingredients"
+          className={`recipe-form-ingredients ${
+            carouselPage !== "Ingredients" && "hide-section"
+          }`}
+        >
+          <h1>Ingredients</h1>
+          <ul>
+            {ingredientFields.map((ingredient, index) => {
+              return (
+                <li className="recipe-form-ingredient" key={ingredient.id}>
+                  <div className="recipe-form-ingredient-inputs">
+                    <div className="recipe-form-ingredient-input">
+                      <label>Amount</label>
+                      <input
+                        placeholder="Amount"
+                        {...register(`ingredients.${index}.amount`, {
+                          required: true,
+                        })}
+                      />
                     </div>
-                    <div className="recipe-form-move-btns">
-                      <button
-                        onClick={() => {
-                          ingredientMove(index, index - 1);
-                        }}
-                      >
-                        <BsChevronUp />
-                      </button>
-                      <button
-                        onClick={() => {
-                          ingredientMove(index, index + 1);
-                        }}
-                      >
-                        <BsChevronDown />
-                      </button>
+                    <div className="recipe-form-ingredient-input">
+                      <label>Ingredient</label>
+                      <input
+                        placeholder="Ingredient"
+                        type="text"
+                        {...register(`ingredients.${index}.name`, {
+                          required: true,
+                        })}
+                      />
                     </div>
-                  </li>
-                );
-              })}
-              <li className="recipe-form-add-direction">
-                <button
-                  type="button"
-                  onClick={() => {
-                    ingredientAppend({ amount: "", name: "" });
-                  }}
-                >
-                  <BsPlusLg className="icon" />
-                  <div>Add Ingredient</div>
-                </button>
-              </li>
-            </ul>
-          </section>
-          <section
-            id="Directions"
-            className={`recipe-form-directions ${
-              carouselPage !== "Directions" && "hide-section"
-            }`}
-          >
-            <h1>Directions</h1>
-            <ol>
-              {directionFields.map((direction, index) => {
-                return (
-                  <li className="recipe-form-direction" key={direction.id}>
-                    <h2>Step {index + 1}</h2>
                     <button
-                      className="recipe-form-direction-delete"
+                      className="recipe-form-ingredient-delete"
                       type="button"
-                      onClick={() => directionRemove(index)}
+                      onClick={() => ingredientRemove(index)}
                     >
                       <BsTrash className="recipe-form-delete-icon" />
                     </button>
-                    <textarea
-                      {...register(`directions.${index}.text`, {
-                        required: true,
-                      })}
-                    />
-                    <div className="recipe-form-move-btns">
-                      <button
-                        onClick={() => {
-                          directionMove(index, index - 1);
-                        }}
-                      >
-                        <BsChevronUp />
-                      </button>
-                      <button
-                        onClick={() => {
-                          directionMove(index, index + 1);
-                        }}
-                      >
-                        <BsChevronDown />
-                      </button>
-                    </div>
-                  </li>
-                );
-              })}
-              <li className="recipe-form-add-direction">
-                <button
-                  type="button"
-                  onClick={() => {
-                    directionAppend({ text: "" });
-                  }}
-                >
-                  <BsPlusLg className="icon" />
-                  <div>Add a step</div>
-                </button>
-              </li>
-            </ol>
-          </section>
-        </div>
+                  </div>
+                  <div className="recipe-form-move-btns">
+                    <button
+                      onClick={() => {
+                        ingredientMove(index, index - 1);
+                      }}
+                    >
+                      <BsChevronUp />
+                    </button>
+                    <button
+                      onClick={() => {
+                        ingredientMove(index, index + 1);
+                      }}
+                    >
+                      <BsChevronDown />
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+            <li className="recipe-form-add-direction">
+              <button
+                type="button"
+                onClick={() => {
+                  ingredientAppend({ amount: "", name: "" });
+                }}
+              >
+                <BsPlusLg className="icon" />
+                <div>Add Ingredient</div>
+              </button>
+            </li>
+          </ul>
+        </section>
+        <section
+          id="Directions"
+          className={`recipe-form-directions ${
+            carouselPage !== "Directions" && "hide-section"
+          }`}
+        >
+          <h1>Directions</h1>
+          <ol>
+            {directionFields.map((direction, index) => {
+              return (
+                <li className="recipe-form-direction" key={direction.id}>
+                  <h2>Step {index + 1}</h2>
+                  <button
+                    className="recipe-form-direction-delete"
+                    type="button"
+                    onClick={() => directionRemove(index)}
+                  >
+                    <BsTrash className="recipe-form-delete-icon" />
+                  </button>
+                  <textarea
+                    {...register(`directions.${index}.text`, {
+                      required: true,
+                    })}
+                  />
+                  <div className="recipe-form-move-btns">
+                    <button
+                      onClick={() => {
+                        directionMove(index, index - 1);
+                      }}
+                    >
+                      <BsChevronUp />
+                    </button>
+                    <button
+                      onClick={() => {
+                        directionMove(index, index + 1);
+                      }}
+                    >
+                      <BsChevronDown />
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+            <li className="recipe-form-add-direction">
+              <button
+                type="button"
+                onClick={() => {
+                  directionAppend({ text: "" });
+                }}
+              >
+                <BsPlusLg className="icon" />
+                <div>Add a step</div>
+              </button>
+            </li>
+          </ol>
+        </section>
+      </div>
 
       <footer className="recipe-form-footer">
         <button onClick={() => handleClickScroll("About")} type="button">

fc65d6e67328fe7bfab8b6624cb913359d8e8e44
Jeffrey Stinson <jeffstinson@hotmail.com>
2023-08-14 17:37:28 -0700

    style account page

--- frontend/src/Components/pages/AccountPage.tsx
+++ frontend/src/Components/pages/AccountPage.tsx
@@ -4,6 +4,10 @@ import { logout, selectUser } from "../../slices/userSlice";
 import Modal from "../Modal";
 import * as UsersApi from "../../network/users_api";
 import { useNavigate } from "react-router-dom";
+import "../../Styles/AccountPage.css";
+import { FaLock, FaRegUser, FaTrash } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaPencil } from "react-icons/fa6";
 
 const AccountPage = () => {
   const user = useAppSelector(selectUser);
@@ -20,19 +24,56 @@ const AccountPage = () => {
       alert(error);
     }
   };
-  
+
   return (
-    <div>
-      <h1>Account</h1>
-      <div>{user.username}</div>
-      <div>{user.email}</div>
-      <button
-        onClick={() => {
-          setShowDeleteUserModal(true);
-        }}
-      >
-        Delete Account
-      </button>
+    <section className="account-page">
+      <div>
+        <h1>Account</h1>
+        <div className="account-page-section">
+          <div>
+            <FaRegUser size={24} className="icon" />
+            <span>Username: {user.username}</span>
+          </div>
+          <div>
+            <MdEmail size={24} className="icon" />
+            <span>Email: </span>
+            <span>{user.email}</span>
+          </div>
+        </div>
+      </div>
+      <h1>Account Settings</h1>
+      <div className="account-page-section">
+        <div>
+          <FaPencil size={24} className="icon" />
+          <button
+            onClick={() => {
+              setShowDeleteUserModal(true);
+            }}
+          >
+            Change Username
+          </button>
+        </div>
+        <div>
+          <FaLock size={24} className="icon" />
+          <button
+            onClick={() => {
+              setShowDeleteUserModal(true);
+            }}
+          >
+            Change Password
+          </button>
+        </div>
+        <div>
+          <FaTrash size={24} className="icon" />
+          <button
+            onClick={() => {
+              setShowDeleteUserModal(true);
+            }}
+          >
+            Delete Account
+          </button>
+        </div>
+      </div>
       <Modal
         show={showDeleteUserModal}
         handleClose={() => setShowDeleteUserModal(false)}
@@ -48,7 +89,7 @@ const AccountPage = () => {
           <button onClick={() => setShowDeleteUserModal(false)}>Cancel</button>
         </div>
       </Modal>
-    </div>
+    </section>
   );
 };
 
